c5f0df0e3ccd70862b36834ff57c31a924d17582
acarrasquillo <abimael.carrasquillo@gmail.com>
2014-03-21 20:05:33 -0400

    Client alerts with mysql queries

---
+++ dbqueries.py
@@ -0,0 +1,27 @@
+import MySQLdb as mdb
+import settings
+
+# function to fetch a query from the db
+def dbFunc(querytext):
+	
+	try:
+		# connect with db
+		con = mdb.connect(user = settings.user, passwd = settings.passwd, db = settings.db, host = settings.host)
+		#create cursor
+		cursor = con.cursor()
+		cursor.execute(querytext)
+		# execute the queryq
+		answer = cursor.fetchall()
+		print answer
+		if con:
+			con.close()
+		return answer
+
+	except mdb.Error,e:
+		print "Error %d: %s" % (e.args[0], e.args[1])
+
+# query for all the alerts in the db from the client connecting the app
+def destalerts(ip):
+	querytext = "select inet_ntoa(iphdr.ip_src) as src, inet_ntoa(iphdr.ip_dst) as dst, signature,sig_name, timestamp from event,iphdr,signature where(event.cid,event.sid) = (iphdr.cid,iphdr.sid) and inet_ntoa(ip_dst) = '%s' and event.signature = signature.sig_id" % ip
+	print querytext
+	return dbFunc(querytext)
--- webidsapp.py
+++ webidsapp.py
@@ -1,14 +1,17 @@
 from flask import Flask, url_for, render_template, request, jsonify
+import dbqueries as queries
 
 app = Flask(__name__)
 app.debug = True
+
 
 
 # get the client ip and send it to the dashboard
 @app.route('/', methods = ['GET'])
 def index():
 	ip = str(request.remote_addr)
-	return render_template('index.html', ip = ip)
+	dstalerts = queries.destalerts(ip)
+	return render_template('index.html', dstalerts = dstalerts, ip = ip, alertsnum = len(dstalerts))
 
 
 @app.route('/my_ip', methods = ['GET'])
@@ -17,4 +20,4 @@ def return_ip():
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(host="0.0.0.0")
