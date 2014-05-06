92f09c1929f6173db97f0f1274e3dd6bc312d995
acarrasquillo <abimael.carrasquillo@gmail.com>
2014-05-06 09:16:22 -0400

    Flows Grapher and alerts sum

--- dbqueries.py
+++ dbqueries.py
@@ -22,6 +22,14 @@ def dbFunc(querytext):
 
 # query for all the alerts in the db from the client connecting the app
 def destalerts(ip):
-	querytext = "select inet_ntoa(iphdr.ip_src) as src, inet_ntoa(iphdr.ip_dst) as dst, signature,sig_name, timestamp from event,iphdr,signature where(event.cid,event.sid) = (iphdr.cid,iphdr.sid) and inet_ntoa(ip_dst) = '%s' and event.signature = signature.sig_id" % ip
+	querytext = "select inet_ntoa(iphdr.ip_src) as src, inet_ntoa(iphdr.ip_dst) as dst, signature,sig_name, timestamp from event,iphdr,signature where(event.cid,event.sid) = (iphdr.cid,iphdr.sid) and ((inet_ntoa(ip_dst) = '%s') or (inet_ntoa(ip_src) = '%s')) and event.signature = signature.sig_id order by timestamp DESC" % (ip,ip)
 	print querytext
 	return dbFunc(querytext)
+
+# query to get the sum of diferents signatures detected by the client ip
+
+def sigsum(ip):
+	querytext = "select count(*), sig_name as sig from event,iphdr,signature where(event.cid,event.sid) = (iphdr.cid,iphdr.sid) and ((inet_ntoa(ip_dst) = '%s') or (inet_ntoa(ip_src) = '%s')) and event.signature = signature.sig_id group by sig order by timestamp DESC" % (ip,ip)
+	print querytext
+	return dbFunc(querytext)
+
--- webidsapp.py
+++ webidsapp.py
@@ -1,22 +1,43 @@
 from flask import Flask, url_for, render_template, request, jsonify
-import dbqueries as queries
-
+from MasterGrapherView import *
+import dbqueries as querie
 app = Flask(__name__)
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 app.debug = True
 
 
 
 # get the client ip and send it to the dashboard
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['GET', 'POST'])
+
 def index():
 	ip = str(request.remote_addr)
-	dstalerts = queries.destalerts(ip)
-	return render_template('index.html', dstalerts = dstalerts, ip = ip, alertsnum = len(dstalerts))
+	dstalerts = querie.destalerts(ip)
+	sumsig = querie.sigsum(ip)
+	print sumsig
 
+	if request.method == 'POST':
+	#def graph():
+		type = request.form["type"]
+		filter=request.form["filter"]
+		entity=request.form["entity"]
+		label=request.form["label"]
+		portlabel=request.form["portlabel"]
+		tolabel=request.form["tolabel"]
+		h = request.form["h"]
+		w = request.form["w"]
+		views=request.form["views"]
+		#paths=views.split["#"]
+		
+		return getGraph(type,filter,entity,h,w,portlabel,tolabel,label)
+	
+	return render_template('index.html', dstalerts = dstalerts, ip = ip, alertsnum = len(dstalerts), sumsig = sumsig)
 
 @app.route('/my_ip', methods = ['GET'])
 def return_ip():
 	return jsonify({'ip': request.remote_addr})
+
+#@app.route('/graph', methods = [''])
 
 
 if __name__ == '__main__':
